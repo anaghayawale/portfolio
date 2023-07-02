cea8333ccf781ba9b37d289e1e0350e387e3a083
Anagha Yawale <111850322+anaghayawale@users.noreply.github.com>
2023-07-02 12:29:43 -0700

    create NavButton

---
+++ lib/widgets/nav_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class NavButton extends StatefulWidget {
+  final String text;
+
+  const NavButton({Key? key, required this.text}) : super(key: key);
+
+  @override
+  _NavButtonState createState() => _NavButtonState();
+}
+
+class _NavButtonState extends State<NavButton> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        // Add your button press logic here
+      },
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.resolveWith<Color>(
+          (Set<MaterialState> states) {
+            if (states.contains(MaterialState.hovered)) {
+              return Colors.deepPurple; // Set the desired color for the hover state
+            }
+            return Colors.black; // Set the default color
+          },
+        ),
+        overlayColor: MaterialStateProperty.all<Color>(
+            Colors.transparent), // Set transparent overlay color
+      ),
+      child: Text(
+        widget.text,
+        style: const TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+      ),
+    );
+  }
+}
